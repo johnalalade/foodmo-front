84425cb61b2d63eb1ba0bdc2786ac19c0ce01b5e
kayphrosh <olakunbiolabode@yahoo.com>
2022-04-04 04:47:11 +0100

    Footer Section Added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import HeaderSection from './component/header';
 import AppFeatures from './component/features';
 import HowToDownload from './component/DownloadContainer';
-import Footer from './component/footer'
+import Footer from './component/footer';
 import './index.css';
 
 function App() {
   return (
     <div>
-      <HeaderSection/>
-      <AppFeatures/>
-      <HowToDownload/>
-      <Footer/>
+      <HeaderSection />
+      <AppFeatures />
+      <HowToDownload />
+      <Footer />
     </div>
   );
 }
