80b9dcb2d3e2161ec9b7eb205b9cef6075f73861
kayphrosh <olakunbiolabode@yahoo.com>
2022-04-29 20:38:14 +0100

    done

--- src/component/features.js
+++ src/component/features.js
@@ -11,31 +11,37 @@ function AppFeatures() {
       title: 'Use Your Food Money For Food Only',
       para: 'You can be tempted to divert your food money to other things. This is why you need a foodwallet that is safe, secure and dedicated only to your food needs.',
       link: 'Join the waiting list',
+      imageIllustration: require('../Images/feature-1.svg').default
     },
     {
       title: 'Organize Your Foodlife',
       para: 'You can now centralize your food budget and spending on our platform. We help you manage your spending and reward you with food gifts, giveaway deals, big food loans',
       link: 'Join the waiting list',
+      imageIllustration: require('../Images/feature-1.svg').default
     },
     {
       title: 'Buy Food Now Pay Later.',
       para: 'Just like the telecom companies do with your airtime, if and when you run out of money or with an insufficient fund for your food purchases, we can advance you a food loan that allows you buy the food you need and you pay us back later.',
-      link: 'Join the waiting list'
+      link: 'Join the waiting list',
+      imageIllustration: require('../Images/feature-3.svg').default
     },
     {
       title: 'Save More Money On Food.',
       para: 'Our online marketplaces and offline partner vendors all work to offer you food deals at best prices. For example, Collabo Deals offer you wholesale rates when you team up with others. Same with Super Deals and Giveaway Deals marketplaces.',
-      link: 'Join the waiting list'
+      link: 'Join the waiting list',
+      imageIllustration: require('../Images/feature-4.svg').default
     },
     {
       title: 'Fast &amp; Reliable Delivery ',
       para: 'Our deliveries are fast and flexible using standby delivery bikes and vehicles and better still, buyers can pick up food packages from our nearest drop points to them.',
-      link: 'Join the waiting list'
+      link: 'Join the waiting list',
+      imageIllustration: require('../Images/feature-5.svg').default
     },
     {
       title: 'No Charges On Food Payments',
       para: ' Its now easy to avoid bank troubles and charges when paying for food by sending money from your foodwallet to the sellers wallet as many times as you like for free. ',
-      link: 'Join the waiting list'
+      link: 'Join the waiting list',
+      imageIllustration: require('../Images/feature-1.svg').default
     }
   ];
 
